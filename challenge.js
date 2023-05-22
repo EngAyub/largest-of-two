22bdc85fb1b55fba36b94d9dd5042488a7ab8dd5
ENG AYUUB <moztaphem9@gmal.com>
2023-05-22 18:02:42 +0300

    largest two done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const variable1 = 10;
+const variable2 = 5;
+
+if(variable1 > variable2){
+    console.log("The largest number out of the two numbers is var1 ")
+}else if (variable2 > variable1){
+
+    console.log("The largest number out of the two numbers is var2")
+
+}else{
+    console.log("Both numbers are equal")
+}
+
